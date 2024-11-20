39586988b14aedc3e4dc27994397834260c70949
Avihai Zeligman <avihai789@gmail.com>
2024-11-20 11:29:58 +0200

    Added basic push and sounds

--- Assets/Scripts/Presenter/PinataPresenter.cs
+++ Assets/Scripts/Presenter/PinataPresenter.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
@@ -9,6 +11,10 @@ public class PinataView : MonoBehaviour
     [SerializeField] private Sprite[] _pinataSprites;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip[] _hitSounds;
+    [SerializeField] private AudioClip _explodeSound;
+    [SerializeField] Rigidbody2D _pinataRigidbody;
+    
+    private float pushForce = 10f;
     
     private int pinaSpriteIndex = 0;
     
@@ -21,20 +27,28 @@ public class PinataView : MonoBehaviour
         if (pinaSpriteIndex >= _pinataSprites.Length)
         {
             _pinataImage.gameObject.SetActive(false);
+            PlayHitSound(_explodeSound);
             return;
         }
         _pinataImage.sprite = _pinataSprites[pinaSpriteIndex];
     }
     
-    private void PlayHitSound()
+    private void PlayHitSound(AudioClip hitSound = null)
     {
-        _audioSource.clip = _hitSounds[Random.Range(0, _hitSounds.Length)];
+        _audioSource.clip = hitSound != null ? hitSound : _hitSounds[Random.Range(0, _hitSounds.Length)];
         _audioSource.Play();
     }
     
     public void OnClick()
     {
         PlayHitSound();
+        PushPinata();
         Click?.Invoke();
+    }
+    
+    private void PushPinata()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        _pinataRigidbody.AddForce(randomDirection * pushForce, ForceMode2D.Impulse);
     }
 }
